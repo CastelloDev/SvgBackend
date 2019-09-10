687b18c485be4788eabd1b008c46424ffb084746
Mpilo Mshengu <mmshengu@retrorebbit.co.za>
2019-09-10 11:44:52 +0200

    convert data url to svg ,optimize svg and return data url

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
-// server.js
+import express from 'express';
+import dataUriToBuffer from 'data-uri-to-buffer';
+import bodyParser from 'body-parser';
+import svgToDataURL from 'svg-to-dataurl';
+import { svgo } from "../SvgBackend/src/svgo.config";
+import SVGO from "svgo";
 
-const express = require("express");
 const app = express();
 const port = process.env.PORT || 3500;
+const svgoObject = new SVGO(svgo);
 
-// viewed at http://localhost:3500
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
 app.get("/", function(req, res) {
     res.send("Again I Go Unnoticed");
+});
+
+app.get('/optimizeSvg',function(req,res){
+    svgoObject.optimize(dataUriToBuffer(req.body.dataUrl).toString()).then(result => {
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify({urlData:svgToDataURL(result.data)}));
+  })
+  .catch(err => console.log(err));
 });
   
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));

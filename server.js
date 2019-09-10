ef34cd664c202bfa9053adf86ccf8e7caea27d05
Mpilo Mshengu <mmshengu@retrorebbit.co.za>
2019-09-10 16:33:24 +0200

    enable cors

--- server.js
+++ server.js
@@ -4,13 +4,15 @@ import bodyParser from 'body-parser';
 import svgToDataURL from 'svg-to-dataurl';
 import { svgo } from "../SvgBackend/src/svgo.config";
 import SVGO from "svgo";
+import cors from 'cors'
 
 const app = express();
 const port = process.env.PORT || 3500;
 const svgoObject = new SVGO(svgo);
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
 
 app.post('/optimizeSvg',function(req,res){
     svgoObject.optimize(dataUriToBuffer(req.body.dataUrl).toString())
